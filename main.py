bd702bcad53df4bc6daf196ea8c25b28bcc8a90f
Socrates <74465738+ramenwithparmesancheese@users.noreply.github.com>
2023-12-18 11:38:51 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import random
+
+PIN = random.randint(0, 9)
+WORD = random.randint(0, 26)
+
+
+print(PIN)
+
+if WORD == 0:
+    WORD = "a"
+if WORD == 1:
+    WORD = "b"
+if WORD == 2:
+    WORD = "c"
+if WORD == 3:
+    WORD = "d"
+if WORD == 4:
+    WORD = "e"
+if WORD == 5:
+    WORD = "f"
+if WORD == 6:
+    WORD = "g"
+if WORD == 7:
+    WORD = "h"
+if WORD == 8:
+    WORD = "i"
+if WORD == 9:
+    WORD = "j"
+if WORD == 10:
+    WORD = "k"
+if WORD == 11:
+    WORD = "l"
+if WORD == 12:
+    WORD = "m"
+if WORD == 13:
+    WORD = "n"
+if WORD == 14:
+    WORD = "o"
+if WORD == 15:
+    WORD = "p"
+if WORD == 16:
+    WORD = "q"
+if WORD == 17:
+    WORD = "r"
+if WORD == 18:
+    WORD = "s"
+if WORD == 19:
+    WORD = "t"
+if WORD == 20:
+    WORD = "u"
+if WORD == 21:
+    WORD = "v"
+if WORD == 22:
+    WORD = "w"
+if WORD == 23:
+    WORD = "x"
+if WORD == 24:
+    WORD = "Y"
+if WORD == 25:
+    WORD = "z"
